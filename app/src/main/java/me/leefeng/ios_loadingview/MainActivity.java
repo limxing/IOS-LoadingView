d8ff1981d464c6888fdc49b2e10d8087231ca21d
李利锋 <limxing991@163.com>
2016-07-18 09:39:30 +0800

    Initial commit

---
+++ app/src/main/java/me/leefeng/ios_loadingview/MainActivity.java
@@ -0,0 +1,27 @@
+package me.leefeng.ios_loadingview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final LoadingView load = (LoadingView) findViewById(R.id.load);
+        Switch main_switch = (Switch) findViewById(R.id.main_switch);
+        main_switch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if (b){
+                    load.startLoad();
+                }else{
+                    load.stopLoad();
+                }
+            }
+        });
+    }
+}
