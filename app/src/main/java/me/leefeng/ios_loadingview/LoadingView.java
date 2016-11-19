4665500fbcb4c7daf5272b1f5c813dea020753bd
limxing <474631895@qq.com>
2016-11-19 14:55:40 +0800

    呵呵哒

--- app/src/main/java/me/leefeng/ios_loadingview/LoadingView.java
+++ app/src/main/java/me/leefeng/ios_loadingview/LoadingView.java
@@ -1,12 +1,12 @@
 package me.leefeng.ios_loadingview;
 
 import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.graphics.Matrix;
+
 import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
 import android.widget.ImageView;
+
 
 import java.lang.ref.SoftReference;
 
@@ -17,6 +17,8 @@ public class LoadingView extends ImageView {
     private MyRunable runnable;
     private int width;
     private int height;
+    private Drawable drawable;
+
     public LoadingView(Context context) {
         super(context);
     }
@@ -30,46 +32,56 @@ public class LoadingView extends ImageView {
     }
 
     @Override
-    protected void onDetachedFromWindow() {
-        super.onDetachedFromWindow();
-        runnable.setFlag(false);
-        runnable = null;
-    }
-
-
-    @Override
     protected void onAttachedToWindow() {
         super.onAttachedToWindow();
         init();
+        runnable.startload();
+    }
+
+    /**
+     * 设置静态的图片
+     *
+     * @param res
+     */
+    public void setDrawable(int res) {
+        runnable.stopload();
+        setImageDrawable(getResources().getDrawable(res));
+    }
+
+    public void startLoading() {
+        setImageDrawable(drawable);
+        runnable.startload();
+    }
+
+    @Override
+    protected void onDetachedFromWindow() {
+        super.onDetachedFromWindow();
+        runnable.stopload();
 
     }
 
     private void init() {
         setScaleType(ScaleType.MATRIX);
-       Drawable drawable= getDrawable();
-        if (drawable==null) {
-            Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.loading);
-            setImageBitmap(bitmap);
-            width = bitmap.getWidth() / 2;
-            height = bitmap.getHeight() / 2;
-        }else{
-            measure(0,0);
-            width = getMeasuredWidth() / 2;
-            height = getMeasuredHeight() / 2;
+        drawable = getDrawable();
+        if (drawable == null) {
+            drawable = getResources().getDrawable(R.drawable.loading);
+            setImageDrawable(drawable);
         }
+        measure(0, 0);
+        width = getMeasuredWidth() / 2;
+        height = getMeasuredHeight() / 2;
         runnable = new MyRunable(this);
-        runnable.run();
     }
 
 
     static class MyRunable implements Runnable {
+        private boolean flag;
         private SoftReference<LoadingView> loadingViewSoftReference;
         private float degrees = 0f;
         private Matrix max;
-        private boolean flag=true;
 
-        public MyRunable(LoadingView loadingView){
-            loadingViewSoftReference=new SoftReference<LoadingView>(loadingView);
+        public MyRunable(LoadingView loadingView) {
+            loadingViewSoftReference = new SoftReference<LoadingView>(loadingView);
             max = new Matrix();
         }
 
@@ -77,18 +89,30 @@ public class LoadingView extends ImageView {
         public void run() {
             if (loadingViewSoftReference.get().runnable != null && max != null) {
                 degrees += 30f;
-                max.setRotate(degrees,  loadingViewSoftReference.get().width,  loadingViewSoftReference.get().height);
+                max.setRotate(degrees, loadingViewSoftReference.get().width, loadingViewSoftReference.get().height);
                 loadingViewSoftReference.get().setImageMatrix(max);
                 if (degrees == 360) {
                     degrees = 0;
                 }
-                if (flag)
+                if (flag) {
                     loadingViewSoftReference.get().postDelayed(loadingViewSoftReference.get().runnable, 80);
+                } else {
+                    max.setRotate(0, loadingViewSoftReference.get().width, loadingViewSoftReference.get().height);
+                    loadingViewSoftReference.get().setImageMatrix(max);
+                }
             }
         }
 
-        public void setFlag(boolean flag) {
-            this.flag = flag;
+        public void stopload() {
+            flag = false;
+
+        }
+
+        public void startload() {
+            flag = true;
+            if (loadingViewSoftReference.get().runnable != null && max != null) {
+                loadingViewSoftReference.get().postDelayed(loadingViewSoftReference.get().runnable, 80);
+            }
         }
     }
 }
